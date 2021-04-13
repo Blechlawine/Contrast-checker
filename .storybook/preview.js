5845c301e523a429d395350ca685e5a92d96d33e
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2021-04-13 11:57:35 +0200

    what

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,11 +2,8 @@ import { configure, addDecorator } from '@storybook/vue';
 import { jsxDecorator } from 'storybook-addon-jsx';
 
 addDecorator(jsxDecorator);
-<<<<<<< HEAD
 
 require("../pages/index");
-=======
->>>>>>> f95e0d8af25e19277cc83eb358f80e31b92ad0f5
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
