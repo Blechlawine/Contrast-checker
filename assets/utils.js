a4113c61c1f63bf9f437fdbaa20740df9df63b73
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2021-04-22 14:59:15 +0200

    added copytoclipboard function

--- assets/utils.js
+++ assets/utils.js
@@ -2,4 +2,15 @@ function scale(value, inMin, inMax, outMin, outMax) {
     return (value - inMin) / (inMax - inMin) * (outMax - outMin) + outMin;
 }
 
-export { scale };
+function copyString(value) {
+    let el = document.createElement("textarea");
+    el.value = value;
+    el.setAttribute("readonly", "");
+    el.style = {position: "absolute", left: "-999999px"};
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+}
+
+export { scale , copyString };
