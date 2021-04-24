ff01b9635e99cc50ba701660950aba211beb0331
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2021-04-24 17:04:08 +0200

    added Copyfield to Colorpicker

--- components/Input/CopyField.jsx
+++ components/Input/CopyField.jsx
@@ -5,69 +5,32 @@ import { copyString } from "../../assets/utils.js";
 export default {
     name: "CopyField",
     props: {
-        typ: {
+        value: {
             type: String,
-            default: "hex",
-            validator: (value) => {
-                return ["hex", "rgb", "hsl"].indexOf(value) !== -1;
-            }
+            required: true
         }
     },
     data() {
         return {
-            hex: undefined
         }
     },
     methods: {
         copyToClipboard() {
-            let stuff = "#" + this.$refs.hexInput.value;
-            copyString(stuff);
-        },
-        checkHexString(input) {
-            let validChars = ["a", "b", "c", "d", "e", "f", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-            if (input instanceof KeyboardEvent) {
-                let inputString = input.key;
-                if (!validChars.includes(inputString)) {
-                    input.preventDefault();
-                }
-            } else if (input instanceof ClipboardEvent && input.type === "paste") {
-                let inputString = input.target.value;
-                let paste = (input.clipboardData || window.clipboardData).getData('text');
-                console.log(paste);
-                if (paste.startsWith("#")) {
-                    paste = paste.substring(1);
-                }
-                input.preventDefault();
-                this.$refs.hexInput.value = paste;
-            }
-            console.log(input);
+            copyString(this.value);
         }
     },
     computed: {
         inputWidth() {
-            let width = "";
-            switch (this.typ.toLowerCase()) {
-                case "hex":
-                    width = "6ch";
-                    break;
-                default:
-                    width = "18ch";
-                    break;
-            }
             return {
-                "width": width
+                "width": this.value.length + "ch"
             };
         }
     },
     render(h) {
         return (
             <div class="copyField">
-                <p>
-                    {this.typ === "hex" ? "#" : ""}
-                    <input class="hexInput" style={this.inputWidth} type="text" v-model={this.hex} onKeypress={this.checkHexString} onPaste={this.checkHexString} placeholder="000000" ref="hexInput"/>
-                </p>
+                <p>{this.value}</p>
                 <div class="copyBtn" onClick={this.copyToClipboard}><span class="material-icons middle">content_copy</span></div>
-                {this.validHex === false ? <p>No hex value</p> : null}
             </div>
         );
     }
--- components/Picker/ColorPickerBig.jsx
+++ components/Picker/ColorPickerBig.jsx
@@ -1,8 +1,10 @@
 import SatValPicker from "./SatValPicker.jsx";
 import HueSlider from "../Slider/HueSlider.jsx";
 import Dropdown from "../Input/Dropdown.jsx";
+import CopyField from "../Input/CopyField.jsx";
 
 import {scale} from "../../assets/utils.js";
+import * as chroma from "chroma-js";
 
 import "./ColorPicker.css";
 
@@ -11,7 +13,8 @@ export default {
     components: {
         SatValPicker,
         HueSlider,
-        Dropdown
+        Dropdown,
+        CopyField
     },
     data() {
         return {
@@ -53,12 +56,28 @@ export default {
             this.activeMode = modeIndex;
         }
     },
+    computed: {
+        copyValue() {
+            if (this.sliderModes[this.activeMode].toLowerCase() === "rgb") {
+                return chroma({h: this.hue, s: this.saturation, v: this.value}).css();
+            } else if (this.sliderModes[this.activeMode].toLowerCase() === "hsl") {
+                let hslSat = chroma({h: this.hue, s: this.saturation, v: this.value}).get("hsl.s") * 100;
+                let hslLig = chroma({h: this.hue, s: this.saturation, v: this.value}).get("hsl.l") * 100;
+                return `hsl(${this.hue}, ${hslSat}%, ${hslLig}%)`;
+            } else {
+                return chroma({h: this.hue, s: this.saturation, v: this.value}).hex();
+            }
+        }
+    },
     render(h) {
         return (
             <div class="colorPicker">
                 <SatValPicker handlePosX={this.satValPicker.handlePosX} handlePosY={this.satValPicker.handlePosY} hue={this.hue} saturation={this.saturation} value={this.value} v-on:satValChanged={this.satValChanged}/>
                 <HueSlider handlePosition={this.hueSlider.handlePosition} hue={this.hue} v-on:hueChanged={this.hueChanged}/>
-                <Dropdown values={this.sliderModes} v-on:onSelect={this.sliderModeChanged}/>
+                <div class="horizontalFlex">
+                    <Dropdown values={this.sliderModes} v-on:onSelect={this.sliderModeChanged}/>
+                    <CopyField value={this.copyValue}/>
+                </div>
             </div>
         );
     }
