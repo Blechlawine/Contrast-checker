817713d9c95993f25ef9baa547cd50c3e0d1355e
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2021-05-10 18:11:08 +0200

    color palette generation finished

--- pages/palette.vue
+++ pages/palette.vue
@@ -14,7 +14,7 @@
                     </div>
                     <div class="colorInfo">
                         <p class="colorLabel">
-                            {{color.hex}}
+                            {{getDisplayText(color.hex)}}
                         </p>
                         <span class="material-icons copyIcon" v-on:click="copyColor(color.hex)">content_copy</span>
                         <span :class="pinClasses(color.locked)" :style="`opacity: ${color.locked ? '1' : '0.6'}`" v-on:click="pinColor(color)">push_pin</span>
@@ -54,6 +54,7 @@ export default {
             harmonyValues: [
                 "Auto",
                 "Analogous",
+                "Monochromatic",
                 "Complementary",
                 "Split-complementary",
                 "Triadic",
@@ -71,27 +72,27 @@ export default {
             colors: [
                 {
                     id: 0,
-                    hex: "#E35141",
+                    hex: "#FFFFFF",
                     locked: false
                 },
                 {
                     id: 1,
-                    hex: "#4caf50",
+                    hex: "#FFFFFF",
                     locked: false
                 },
                 {
                     id: 2,
-                    hex: "#ffeb3b",
+                    hex: "#FFFFFF",
                     locked: false
                 },
                 {
                     id: 3,
-                    hex: "#03a9f4",
+                    hex: "#FFFFFF",
                     locked: false
                 },
                 {
                     id: 4,
-                    hex: "#03a9f4",
+                    hex: "#FFFFFF",
                     locked: false
                 }
             ],
@@ -100,6 +101,22 @@ export default {
         }
     },
     methods: {
+        getDisplayText(hexIn) {
+            switch(this.selectedDisplayType.toLowerCase()) {
+                case "hsl":
+                    let hue = Math.round(chroma(hexIn).get("hsl.h"));
+                    let sat = Math.round(chroma(hexIn).get("hsl.s") * 100);
+                    let lig = Math.round(chroma(hexIn).get("hsl.l") * 100);
+                    return `hsl(${hue}, ${sat}%, ${lig}%)`
+                    break;
+                case "rgb":
+                    return chroma(hexIn).css();
+                    break;
+                case "hex":
+                    return hexIn;
+                    break;
+            }
+        },
         changeHarmony(valueIndex) {
             this.harmony = this.harmonyValues[valueIndex].toLowerCase();
             this.generateColorsForSelectedHarmony();
@@ -126,7 +143,44 @@ export default {
             }
             return lockedColors;
         },
-        generateRandomColors(startIndex) {
+        getUnlockedColors() {
+            let unlocked = [];
+            for (let k = 0; k < this.colors.length; k++) {
+                if (!this.colors[k].locked) unlocked.push(this.colors[k]);
+            }
+            return unlocked;
+        },
+        generateAnalogousColors(startIndex, startColor) {
+            // TODO: überarbeiten
+            let startHue = startColor.get("hsl.h");
+            let angles = [[startHue, startHue + 20, startHue + 40], [startHue, startHue - 20, startHue - 40]];
+            let angleArray = angles[Math.round(Math.random())];
+            this.generateColorsWithFixedHueOffset(startColor, angleArray);
+        },
+        generateMonochromaticColors(startIndex, startColor) {
+            let startHue = startColor.get("hsl.h");
+            let angleArray = [startHue];
+            this.generateColorsWithFixedHueOffset(startColor, angleArray);
+        },
+        generateComplementaryColors(startIndex, startColor) {
+            let startHue = startColor.get("hsl.h")
+            let angles = [[startHue, startHue + 180], [startHue, startHue - 180]];
+            let angleArray = angles[Math.round(Math.random())];
+            this.generateColorsWithFixedHueOffset(startColor, angleArray);
+        },
+        generateSplitComplementaryColors(startIndex, startColor) {
+            let startHue = startColor.get("hsl.h");
+            let angles = [[startHue, startHue + 171, startHue + 99], [startHue, startHue - 171, startHue - 99]];
+            let angleArray = angles[Math.round(Math.random())];
+            this.generateColorsWithFixedHueOffset(startColor, angleArray);
+        },
+        generateTriadicColors(startIndex, startColor) {
+            let startHue = startColor.get("hsl.h");
+            let angles = [[startHue, startHue + 120, startHue + 240], [startHue, startHue - 120, startHue - 240]];
+            let angleArray = angles[Math.round(Math.random())];
+            this.generateColorsWithFixedHueOffset(startColor, angleArray);
+        },
+        generateRandomColors(startIndex, startColor) {
             for (let k = startIndex; k < this.colors.length; k++) {
                 let color = this.colors[k];
                 if (!color.locked) {
@@ -134,9 +188,66 @@ export default {
                 }
             }
         },
+        generateSquareColors(startIndex, startColor) {
+            let startHue = startColor.get("hsl.h");
+            let angles = [[startHue, startHue + 90, startHue + 180, startHue + 270], [startHue, startHue - 90, startHue - 180, startHue - 270]];
+            let angleArray = angles[Math.round(Math.random())];
+            this.generateColorsWithFixedHueOffset(startColor, angleArray);
+        },
+        generateTetradicColors(startIndex, startColor) {
+            let startHue = startColor.get("hsl.h");
+            let angles = [[ startHue, 60 + startHue, 180 + startHue, 240 + startHue], [startHue, startHue - 60, startHue - 180, startHue - 240]];
+            let angleArray = angles[Math.round(Math.random())];
+            this.generateColorsWithFixedHueOffset(startColor, angleArray);
+        },
+        generateColorsWithFixedHueOffset(startColor, hues) {
+            let unlocked = this.getUnlockedColors();
+            for (let i = 0; i < unlocked.length; i++) {
+                let color = unlocked[i];
+                if (!color.locked) {
+                    let hue = startColor.get("hsl.h");
+                    if (i < hues.length) {
+                        hue = (hues[i]) % 360;
+                    } else {
+                        hue = (hues[Math.round(Math.random() * hues.length - 1)]) % 360;
+                    }
+                    let sat = (Math.random());
+                    let lig = (Math.random());
+                    color.hex = chroma(hue, sat, lig, "hsl").hex();
+                }
+            }
+        },
+        generateWarmColors(startIndex, startColor) {
+            if (this.getLockedColors().length == 0) {
+                startIndex = 0;
+            }
+            for (let k = startIndex; k < this.colors.length; k++) {
+                let color = this.colors[k];
+                if (!color.locked) {
+                    let hue = (Math.random() * 110 + 310) % 360;
+                    let sat = (Math.random());
+                    let lig = (Math.random());
+                    color.hex = chroma(hue, sat, lig, "hsl").hex();
+                }
+            }
+        },
+        generateCoolColors(startIndex, startColor) {
+            if (this.getLockedColors().length == 0) {
+                startIndex = 0;
+            }
+            for (let k = startIndex; k < this.colors.length; k++) {
+                let color = this.colors[k];
+                if (!color.locked) {
+                    let hue = (Math.random() * 250 + 60) % 360;
+                    let sat = (Math.random());
+                    let lig = (Math.random());
+                    color.hex = chroma(hue, sat, lig, "hsl").hex();
+                }
+            }
+        },
         generateColorsForSelectedHarmony() {
             // https://www.luminous-landscape.com/color-harmonies-4-cool-warm-split-tetradic-and-square/
-            let startColor;
+            let startColor = null;
             let locked = this.getLockedColors();
             let startIndex = 0;
             if (locked.length == 0) {
@@ -149,77 +260,58 @@ export default {
             }
             switch (this.harmony) {
                 case "auto":
+                    let prob = Math.random() * 10;
+                    if(prob < 1) {
+                        this.generateRandomColors(startIndex, startColor);
+                    } else if (prob < 2) {
+                        this.generateAnalogousColors(startIndex, startColor);
+                    } else if (prob < 3) {
+                        this.generateTriadicColors(startIndex, startColor);
+                    } else if (prob < 4) {
+                        this.generateTetradicColors(startIndex, startColor);
+                    } else if (prob < 5) {
+                        this.generateComplementaryColors(startIndex, startColor);
+                    } else if (prob < 6) {
+                        this.generateSplitComplementaryColors(startIndex, startColor);
+                    } else if (prob < 7) {
+                        this.generateSquareColors(startIndex, startColor);
+                    } else if (prob < 8) {
+                        this.generateWarmColors(startIndex, startColor);
+                    } else if (prob < 9) {
+                        this.generateCoolColors(startIndex, startColor);
+                    } else {
+                        this.generateMonochromaticColors(startIndex, startColor);
+                    }
                     break;
                 case "random":
-                    this.generateRandomColors(startIndex);
+                    this.generateRandomColors(startIndex, startColor);
                     break;
                 case "analogous":
-                    for (let k = startIndex; k < this.colors.length; k++) {
-                        let color = this.colors[k];
-                        if (!color.locked) {
-                            let hue = startColor.get("hsl.h");
-                            let sat = (Math.random());
-                            let lig = (Math.random());
-                            color.hex = chroma(hue, sat, lig, "hsl").hex();
-                        }
-                    }
+                    this.generateAnalogousColors(startIndex, startColor);
                     break;
                 case "triadic":
-                    for (let k = startIndex; k < this.colors.length; k++) {
-                        let color = this.colors[k];
-                        if (!color.locked) {
-                            let prob = Math.random();
-                            let hue = startColor.get("hsl.h");
-                            if (prob < 0.33) {
-                                hue += 240;
-                            } else if (prob < 0.66) {
-                                hue += 120;
-                            }
-                            hue %= 360;
-                            let sat = (Math.random());
-                            let lig = (Math.random());
-                            color.hex = chroma(hue, sat, lig, "hsl").hex();
-                        }
-                    }
+                    this.generateTriadicColors(startIndex, startColor);
                     break;
                 case "tetradic":
+                    this.generateTetradicColors(startIndex, startColor);
                     break;
                 case "complementary":
-                    for (let k = startIndex; k < this.colors.length; k++) {
-                        let color = this.colors[k];
-                        if (!color.locked) {
-                            let hue = (startColor.get("hsl.h") + (Math.random() < 0.5 ? 180 : 0)) % 360;
-                            let sat = (Math.random());
-                            let lig = (Math.random());
-                            color.hex = chroma(hue, sat, lig, "hsl").hex();
-                        }
-                    }
+                    this.generateComplementaryColors(startIndex, startColor);
                     break;
                 case "split-complementary":
+                    this.generateSplitComplementaryColors(startIndex, startColor);
                     break;
                 case "square":
-                    for (let k = startIndex; k < this.colors.length; k++) {
-                        let color = this.colors[k];
-                        if (!color.locked) {
-                            let prob = Math.random();
-                            let hue = startColor.get("hsl.h");
-                            if (prob < 0.25) {
-                                hue += 90;
-                            } else if (prob < 0.5) {
-                                hue += 180;
-                            } else if (prob < 0.75) {
-                                hue += 270;
-                            }
-                            hue %= 360;
-                            let sat = (Math.random());
-                            let lig = (Math.random());
-                            color.hex = chroma(hue, sat, lig, "hsl").hex();
-                        }
-                    }
+                    this.generateSquareColors(startIndex, startColor);
                     break;
                 case "warm":
+                    this.generateWarmColors(startIndex, startColor);
                     break;
                 case "cool":
+                    this.generateCoolColors(startIndex, startColor);
+                    break;
+                case "monochromatic":
+                    this.generateMonochromaticColors(startIndex, startColor);
                     break;
                 default:
                     break;
